a0187bf80bedee61c634e235c5ede2bbd44ff347
cosmic <dehara.201002276@gmail.com>
2015-07-14 16:14:36 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+		//ejercicio 1
+		//$('div').fadeOut('slow');
+
+		//ejercicio2
+		/*
+		$('div').on('click', function() {
+				$('div').fadeOut('slow');
+		});
+		*/
+
+		//ejericio3
+		/*
+		$('div').hover(function() {
+				$('div').addClass('red');
+		});
+		*/
+
+		//ejercicio4
+		/*
+    $('div').dblclick(function() {
+        $(this).fadeOut('fast');
+    });
+		*/
+
+		//ejercicio5
+		/*
+		$('div').hover(
+				function(){
+						$(this).addClass('red');
+				},
+				function(){
+						$(this).removeClass('red');
+				}
+		);
+		*/
+		
+		//ejericio6
+		/*
+		$(document).keydown(function(key) {
+        switch(parseInt(key.which,10)) {
+						// Left arrow key pressed
+				case 37:
+						$('img').animate({left: "-=10px"}, 'fast');
+						break;
+						// Up Arrow Pressed
+				case 38:
+						$('img').animate({top: "-=10px"}, 'fast');
+						// Put our code here
+						break;
+						// Right Arrow Pressed
+				case 39:
+						$('img').animate({left: "+=10px"}, 'fast');
+						// Put our code here
+						break;
+						// Down Arrow Pressed
+				case 40:
+						$('img').animate({top: "+=10px"}, 'fast');
+						// Put our code here
+						break;
+				}
+		});
+		*/
+
+});
